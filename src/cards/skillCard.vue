b3056fb98168b58d74b78c1ef4a3fe9168b01e38
Yuuuuuuuuta <y.mitsu92@gmail.com>
2019-06-19 00:37:02 +0900

    skillCard実装

--- src/cards/skillCard.vue
+++ src/cards/skillCard.vue
@@ -1,23 +1,33 @@
 <template>
-  <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card hover>
-        <v-card-title primary-title>
-          <div>
-            <h3>{{ frontEndData.title }}</h3>
-          </div>
-        </v-card-title>
+  <v-card hover>
+    <v-card-title primary-title>
+      {{ skillData.title }}
+    </v-card-title>
+    <v-card-text>{{ skillData.content }}</v-card-text>
+    <v-card-text>主な技術</v-card-text>
+    <v-list>
+      <v-list-tile v-for="skill in skillData.mainSkill" :key="skill.skillName">
+        {{ skill.skillName }}
+      </v-list-tile>
+    </v-list>
+    <!-- <v-dialog v-model="dialog" scrollable max-width="80%">
+      <v-card>
+        <v-card-title>{{ skillData.title }}</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text height="300px">{{ skillData.content }}</v-card-text>
       </v-card>
-    </v-flex>
-  </v-layout>
+    </v-dialog> -->
+  </v-card>
 </template>
 
 <script>
 export default {
   name: 'skillCard',
-  props: [ 'frontEndData' ],
+  props: [ 'skillData' ],
   data () {
-    
+    return {
+      dialog: false
+    }
   }
 }
 </script>
--- src/pages/Skill.vue
+++ src/pages/Skill.vue
@@ -1,14 +1,18 @@
 <template class="Skill">
-  <v-parallax dark height="824">
+  <!-- <v-parallax dark height="824"> -->
+    <div style="height: 80vh">
     <div class="title">
       <h1 style="color:black">Skill</h1>
     </div>
-    <v-layout align-center justify-center>
-      
-      <!-- 表示させる内容ををDialogコンポーネントへ渡す -->
-        <skillCard :frontEndData="frontEnd"></skillCard>
-    </v-layout>
-  </v-parallax>
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex v-for="skill in skillList" :key="skill.skillId" xs6>
+          <skillCard :skillData = "skill" ></skillCard>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    </div>
+  <!-- </v-parallax> -->
 </template>
 
 <script>
@@ -21,22 +25,69 @@ export default {
   },
   data () {
     return {
-      frontEnd: {
+      skillList: [
+      {
+        skillId: 1,
         title: 'フロントエンド',
-        content: '直近の案件ではVue.jsで開発を行なっていました',
-        languages: [
-          {langName: 'JavaScript', level: '2'},
-          {langName: 'Vue.js', level: '3'}
+        content: '直近の案件ではVue.jsで業務アプリのフロント部分の開発を行なっていました',
+        mainSkill: [
+          {skillName: 'JavaScript', level: '2'},
+          {skillName: 'Vue.js', level: '3'}
         ]
       },
-      backEnd: {
+      {
+        skillId: 2,
         title: 'バックエンド',
-        content: '直近の案件ではJavaで開発を行なっていました',
-        languages: [
-          {langName: 'Java', level: '3'},
-          {langName: 'Python', level: '1'}
+        content: '直近の案件ではJavaを使ってAPIの開発を行なっていました',
+        mainSkill: [
+          {skillName: 'Java', level: '3'},
+          {skillName: 'Python', level: '1'}
+        ]
+      },
+      {
+        skillId: 3,
+        title: 'インフラ',
+        content: '業務でのネットワーク機器の設定・構築の経験があります。',
+        mainSkill: [
+          {skillName: 'ルータ(Cisco,Yamaha)', level: '1'},
+          {skillName: 'FireWall', level: '1'},
+          {skillName: 'Proxyサーバ', level: '1'},
+          {skillName: '等', level: '1'}
+        ]
+      },
+      {
+        skillId: 4,
+        title: 'クラウド',
+        content: '現在はクラウドサービスとしてAWSとGCP、コンテナ技術としてDockerを学習中です。',
+        mainSkill: [
+          {skillName: 'AWS', level: '1'},
+          {skillName: 'GCP', level: '1'},
+          {skillName: 'Docker', level: '1'}
+        ]
+      },
+      {
+        skillId: 5,
+        title: 'その他',
+        content: 'SIerとして3年の業務経験があります。要件定義から提案、運用、保守まで一通りの経験があります。お客様との折衝や海外の担当者と連携したサービスのグローバル展開の経験があります。',
+        mainSkill: [
+          {skillName: 'Git', level: '1'},
+          {skillName: 'Redmine', level: '1'},
+          {skillName: '各種Office製品', level: '1'}
+        ]
+      },
+      {
+        skillId: 6,
+        title: '所有資格',
+        content: '直近3年以内に取得した資格は以下となります。',
+        mainSkill: [
+          {skillName: '応用情報技術者', level: '1'},
+          {skillName: 'セキュリティスペシャリスト', level: '1'},
+          {skillName: 'Oracle認定 Java Programmer Silver SE 8', level: '1'},
+          {skillName: 'ORACLE MASTER Bronze 12c(SQL,DBA) ', level: '1'},
+          {skillName: 'TOEIC600点', level: '1'}
         ]
       }
+      ]
     }
   }
 }
